03cbd8fa512db65966702827654c69e56e774f9a
Grzegorz Strzeszewski <grzesstrzeszewski@gmail.com>
2025-01-16 17:12:49 +0100

    refactor(shared): renamed of authSuccessResponse file.

--- packages/shared/index.ts
+++ packages/shared/index.ts
@@ -2,4 +2,4 @@ export * from "./schemas/localUser";
 export * from "./schemas/localCredentials";
 export * from "./schemas/baseResponse";
 export * from "./schemas/signedInUser";
-export * from "./schemas/authSuccessResponse";
+export * from "./schemas/authSuccessResponseSchema";
